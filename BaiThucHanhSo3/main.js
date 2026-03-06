18cc1946db8934ed579f89b20ecb758212cf052d
dinhquangduy2002 <dinhquangduy2002@gmail.com>
2026-03-06 08:16:53 +0700

    add bài thực hành

---
+++ BaiThucHanhSo3/main.js
@@ -0,0 +1,61 @@
+// console.log("Hello from JavaScript!");
+// let age = 20;
+// const name = "An";
+// let isStudent = true;
+//
+// console.log(typeof age);
+// console.log(typeof name);
+// console.log(typeof isStudent);
+//
+// console.log(1 + 2 * 3);
+// console.log("Hello " + "world");
+
+let name = "Duy";
+let yearOfBirth = 2002;
+let currentYear = 2026;
+let age = currentYear - yearOfBirth;
+
+console.log("Xin chào, mình là " + name + ", năm nay mình " + age + " tuổi.");
+
+// let score = 6;
+// if (score >= 8) {
+//     console.log("Giỏi");
+// } else if (score >= 6.5) {
+//     console.log("Khá");
+// } else if (score >= 5) {
+//     console.log("Trung bình");
+// } else {
+//     console.log("Yếu");
+// }
+
+// function tinhDiemTrungBinh(m1, m2, m3) {
+//     let avg = (m1 + m2 + m3) / 3;
+//     return avg;
+// }
+// // console.log(tinhDiemTrungBinh(8,7,9))
+//
+// function xepLoai(avg) {
+//     if (avg >= 8) {
+//         return "Giỏi";
+//     } else if (avg >= 6.5) {
+//         return "Khá";
+//     } else if (avg >= 5 ) {
+//         return "Trung bình";
+//     } else {
+//         return "Yếu";
+//     }
+// }
+// let avg = tinhDiemTrungBinh(8, 7, 9);
+// let loai = xepLoai(avg);
+// console.log("Điểm TB:", avg, " - Xếp loại:", loai);
+
+// function kiemTraTuoi(age) {
+//     if (age >= 18){
+//         console.log("Đủ 18 tuổi")
+//     }
+//     else {
+//         console.log("Chưa đủ 18 tuổi ")
+//     }
+// }
+// console.log(kiemTraTuoi(30))
+// console.log(kiemTraTuoi(16))
